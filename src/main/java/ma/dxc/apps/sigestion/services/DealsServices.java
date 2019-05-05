f9c1cf998df923741325420b4b7d3d4602c888af
Youssef MAHTAT <youssef.mahtat@gmail.com>
2019-05-05 01:48:47 +0100

    Update DealsServices.java

--- src/main/java/ma/dxc/apps/sigestion/services/DealsServices.java
+++ src/main/java/ma/dxc/apps/sigestion/services/DealsServices.java
@@ -13,6 +13,12 @@ public class DealsServices {
         deals.add(new Deal("154865","COCA COLA", "plum spum trum"));
         deals.add(new Deal("587575","ZKIKA", "khrum zbum trum"));
         deals.add(new Deal("855264","TRIMA", "klwa zfhh cfgfg"));
+        deals.add(new Deal("456456","CGY", "plum rfgttgg rfgg"));
+        deals.add(new Deal("865785","SQLy", "khsfdf dfv gh rum"));
+        deals.add(new Deal("855784","CGYEM", "cvdfv dfg fgdgb"));
+        deals.add(new Deal("587515","ALOS", "plsfvfvb gbhg um"));
+        deals.add(new Deal("582575","MOKAS", "khrum sdf dfgdg dgv"));
+        deals.add(new Deal("145864","TETAS", "klw cvcv a zdgdgv"));
     }
 
     public List<Deal> getDeals(){
